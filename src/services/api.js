0d6b39d1c47a6f8fc82c0eff3c899de2bcb2fe71
Romulo Gomes <romulo.gomes@cscglobal.com>
2025-09-23 20:15:06 -0300

    change base api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_URL = 'https://broworkout.back.brothertec.com.br';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
