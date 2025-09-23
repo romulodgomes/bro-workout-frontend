431c810f787dc2993a610c09faa9022a47343338
Romulo Gomes <romulo.gomes@cscglobal.com>
2025-09-23 18:03:24 -0300

    Revert "try to fix routing issues"
    This reverts commit 27d004d4fe874cc1e1a38c979c28f45d4d8863a9.

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? '/api/proxy' 
-  : 'http://157.245.167.2:4000';
+const API_BASE_URL = process.env.API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
